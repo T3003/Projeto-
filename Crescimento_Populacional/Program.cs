26fa26cea922222f0f45957f9f07e3c2f212ec54
T3003 <150155010+T3003@users.noreply.github.com>
2023-11-28 09:11:37 -0300

    Add files via upload

---
+++ Crescimento_Populacional/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira a população inicial: ");
+        double populacaoInicial = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira a população final: ");
+        double populacaoFinal = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira o tempo decorrido (em anos): ");
+        double tempo = double.Parse(Console.ReadLine());
+
+        double taxaDeCrescimento = Math.Pow(populacaoFinal / populacaoInicial, 1 / tempo) - 1;
+
+        Console.WriteLine("A taxa de crescimento populacional é de {0:P2} ao ano.", taxaDeCrescimento);
+    }
+}
